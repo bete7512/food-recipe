77fb5dc25d95854ed5a3ec00ea0d64001b98e670
bete7512 <betekbebe@gmail.com>
2022-08-21 09:48:02 +0300

    go

--- front-end/src/components/authenticated/create-recipe.vue
+++ front-end/src/components/authenticated/create-recipe.vue
@@ -1,14 +1,79 @@
 <template>
-<div>
-    <h1>create recipe here</h1>
-</div>
-<div>
-    hello here am alone
-</div>
+    <div class="flex justify-center">
+        <div class="align-middle ">
+            <div class="popup-content align-middle">
+                <h2 class="text-center text-lg font-bold pt-2">Add new recipe</h2>
+
+                <form @submit.prevent="">
+                    <div class="grid sm:grid-cols-1 lg:grid-cols-2 space-x-10">
+                        <div class="">
+                            <div class="">
+                                <div class="text-2xl font  border-b-gray-900 bg">Title</div>
+                                <input type="text" class="border-2 px-3 bg-slate-300 border-black h-10 w-80 rounded "
+                                    placeholder="recipe title" v-model="title" />
+                            </div>
+                            <div class="">
+                                <div class="text-2xl font  border-b-gray-900">Duration</div>
+                                <input type="text" class="border-2 px-3 bg-slate-300 border-black h-10 w-80 rounded" v-model="title" />
+                            </div>
+                            <div class="w-80">
+                                <label for="countries"
+                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Select Categories</label>
+                                <select id="categories"
+                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                    <option selected>Choose a categories</option>
+                                    <option value="US">breakfast</option>
+                                    <option value="CA">lunch</option>
+                                    <option value="FR">dinner</option>
+                                </select>
+                            </div>
+                            <div class="group  w-80">
+                                <div class="text-2xl font  border-b-gray-900">Description</div>
+                                <textarea class="border-2 p-3 bg-slate-300 border-black h-40 w-80 rounded"
+                                    v-model="description"></textarea>
+                            </div>
+
+                            <div class="">
+                                <div class="text-2xl font  border-b-gray-900">Ingredients</div>
+                                <input type="text" class="border-2 p-2 bg-slate-300 border-black h-10 w-96 rounded" v-model="title" />
+                                <button
+                                    class="flex items-center justify-center w-auto p-10 py-4 my-3 text-base font-medium text-center text-white transition duration-500 ease-in-out transform bg-blue-600 rounded-xl hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">add
+                                    ingridient</button>
+                            </div>
+                        </div>
+                        <div class="">
+                            <div class="group">
+                                <div class="text-2xl font  border-b-gray-900">Instructions</div>
+                                <textarea class="border-2 p-3 bg-slate-300 border-black h-40 w-96 rounded"
+                                    v-model="description"></textarea>
+                                <button
+                                    class="flex items-center p-7 justify-center w-auto  py-4 my-3 text-base font-medium text-center text-white transition duration-500 ease-in-out transform bg-blue-600 rounded-xl hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">add
+                                    instructions</button>
+                            </div>
+                            <div class="">
+                                <div class="text-2xl font  border-b-gray-900">images</div>
+                                <input type="file" class="border-2 bg-slate-300 border-black h-10 w-96 rounded" />
+                                <button
+                                    class="flex items-center justify-center w-auto p-10 py-4 my-3 text-base font-medium text-center text-white transition duration-500 ease-in-out transform bg-blue-600 rounded-xl hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">add
+                                    images</button>
+                            </div>
+                        </div>
+                    </div>
+                    <button
+                        class="flex align-middle items-center justify-center w-auto p-10 py-4 my-3 text-base font-medium text-center text-white transition duration-500 ease-in-out transform bg-blue-600 rounded-xl hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">submit</button>
+                </form>
+            </div>
+        </div>
+    </div>
 </template>
 <script setup lang="ts">
-
+import { ref } from 'vue'
+const title = ref('')
+const duration = ref('')
+const description = ref('')
+const instructions = ref([])
+const ingredients = ref([])
+const images = ref([])
 </script>
-<style>
-
+<style scoped>
 </style>
--- front-end/src/components/authenticated/main.vue
+++ front-end/src/components/authenticated/main.vue
@@ -3,16 +3,21 @@
         <h1><span class="text-yellow-900">Discover</span> Recipe</h1>
     </div>
     <div class="flex justify-between p-3 px-5 shadow-lg">
-        <button class="flex p-4 text-orange-700 hover:bg-slate-400 rounded"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus"
+        <button class="flex p-4 text-orange-700 hover:bg-slate-400 rounded">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus"
                 viewBox="0 0 16 16">
                 <path
                     d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
             </svg>
-            <router-link to="/main/addrecipe"><span>Post an event</span></router-link>
+            <router-link :to="{name:'addrecipe'}"><span>Create Recipe</span></router-link>
             </button>
             <div class="flex space-x-3 ">
-                <button  class="flex p-4 px-8 hover:bg-slate-400 rounded">favorite</button>
-                <button  class="flex p-4 px-8 hover:bg-slate-400 rounded">feeds</button>
+                <router-link :to="{name:'favorite'}">
+                    <button  class="flex p-4 px-8 hover:bg-slate-400 rounded">favorite</button>
+                </router-link>
+                <router-link :to="{name:'feeds'}">
+                    <button  class="flex p-4 px-8 hover:bg-slate-400 rounded">feeds</button>
+                </router-link>
             </div>
         <button >
             <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-person"
