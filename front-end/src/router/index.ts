73139a24c32363a7e8e2f77e92b2084679696f47
bete7512 <betekbebe@gmail.com>
2022-08-21 05:50:19 +0300

    here

--- front-end/src/router/index.ts
+++ front-end/src/router/index.ts
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../components/unauthenticated/Home.vue'
 import login from '../components/unauthenticated/login.vue'
 import signup from '../components/unauthenticated/signup.vue'
+import feeds from '../components/authenticated/feeds.vue'
+import saved from '../components/authenticated/favorite.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -19,6 +21,16 @@ const router = createRouter({
       name:'signup',
       path:'/signup',
       component:signup
+    },
+    {
+      name:'feeds',
+      path:'/feeds',
+      component:feeds
+    },
+    {
+      name:'favorite',
+      path:'saved recipe',
+      component:saved
     }
   ]
 })
