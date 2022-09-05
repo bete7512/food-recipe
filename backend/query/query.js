8446b3b8602c3085f439925bb2b80026f50dcfcf
bete7512 <betekbebe@gmail.com>
2022-09-05 12:56:34 +0300

    remove trash

--- backend/query/query.js
+++
@@ -1,9 +0,0 @@
-const HASURA_OPERATION = `
-query signin($username:String!,$password:String!) {
-  users(where: {email: {_eq: $username}, password: {_eq: $password}}) {
-    id
-  }
-}
-`;
-
-module.exports =  HASURA_OPERATION
