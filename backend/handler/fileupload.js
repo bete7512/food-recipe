387b08e64dca3181ad8a54f9f63a164c0b9085cf
bete7512 <betekbebe@gmail.com>
2022-10-07 09:05:12 +0300

    go for trial

--- backend/handler/fileupload.js
+++ backend/handler/fileupload.js
@@ -1,8 +1,12 @@
+const cloudinary = require('../cloudinary/cloudinary')
+
 const handler = async (req, res) => {
     try {
       const { name, type, base64str } = req.body.input;
       let filebuffer = Buffer.from(base64str, 'base64')
-  
+      console.log(filebuffer);
+      const result = await cloudinary.uploader.upload(req.file.path);
+      console.log(result);
       return res.json({
         file_path: `http://localhost:4000/${name}`
       })
