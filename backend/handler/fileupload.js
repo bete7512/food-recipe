46cbc19ecd38249ecfc41a52371441ec5b9359f7
bete7512 <betekbebe@gmail.com>
2022-10-07 09:59:55 +0300

    try test

--- backend/handler/fileupload.js
+++ backend/handler/fileupload.js
@@ -3,10 +3,13 @@ const upload = require('../cloudinary/multer')
 console.log(upload);
 const handler = async (req, res) => {
     try {
+
+      
       const { name, type, base64str } = req.body.input;
       let filebuffer = Buffer.from(base64str, 'base64')
       console.log(filebuffer);
-      const result = await cloudinary.uploader.upload(req.file.path);
+      const result = await cloudinary.uploader.upload(`data:image/jpg;${base64str}...`)
+      // const result = await cloudinary.uploader.upload(req.file.path);
       console.log(result);
       return res.json({
         file_path: `http://localhost:4000/${name}`
