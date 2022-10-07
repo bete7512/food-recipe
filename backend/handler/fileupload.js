9a6f8b3743d547302bbfc88059586c1c6a739a5a
bete7512 <betekbebe@gmail.com>
2022-10-07 22:04:48 +0300

    trial

--- backend/handler/fileupload.js
+++ backend/handler/fileupload.js
@@ -1,7 +1,7 @@
 const cloudinary = require('../cloudinary/cloudinary')
 const handler = async (req, res) => {
     try {
-      const { name,type,base64str } = req.body
+      const { name,type,base64str } = req.body.input
       const result = await cloudinary.uploader.upload(`data:image/jepg;base64,${base64str}`)
       console.log(result.url)
       return res.json({
